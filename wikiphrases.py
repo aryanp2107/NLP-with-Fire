a7f36706b963396c9b7d8e6416878db9c2036776
Aryan Patel <108725292+aryanp2107@users.noreply.github.com>
2026-04-02 11:55:47 -0400

    Fix main block syntax and remove unused import

--- wikiphrases.py
+++ wikiphrases.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 import fire
-import hello
 from nlplogic.corenlp import get_phrases
 
 
-
-if __name__ == '__main__':
-  fire.Fire(get_phrases)
+if __name__ == "__main__":
+    fire.Fire(get_phrases)
